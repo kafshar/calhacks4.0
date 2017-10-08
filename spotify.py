2df9828eea24132cd17c5ce32e667db9c3f93b10
Christopher Iba <chrisiba@airbears2-10-142-162-77.airbears2.1918.berkeley.edu>
2017-10-07 23:52:14 -0700

    Spotify finally works

--- spotify.py
+++ spotify.py
@@ -1,7 +1,8 @@
 import spotipy
 import spotipy.util as util
-token = util.prompt_for_user_token('1267938772','user-library-modify',client_id='ca09ab003299448ab17d3fffd5aaa141',client_secret='e74a619414dd4fadbd6b47f7b73b9d88',redirect_uri='http://localhost:8888/callback/')
-name = 'Kanye West'
+token = util.prompt_for_user_token('1267938772','user-library-modify',client_id='a3d608b405554a9a9735da2be6e12f08',client_secret='76727e6600c2436f92b6a59833c0703b',redirect_uri='http://localhost/')
+#token = 'BQBr2PUnWurUyp1Wq2vYex73YQF9cglRnBiEYC22suHohLPXzjBzFdRM0oA-r2Eq129g50u5dMdnbNr5TvitqiqnIirUBqeNtD2Mzj_CKgY6QQR6CSYOjfEShqjfeEgff2cKIzdBmAxs6BC9eCUdEO-rTP_PKjE&refresh_token=AQC1p7rOI-wW10vwe0mrZi3BUJKdTmpE1wsw0nxtT-ei3OaJ2yqTdqntqZ67grOj7D2SsSYMCnkb8KFM2dO39IKWInMH_NKiZRY2Lt63xm6doBcPUkUmCQWFfzbQr-KfZ-Y'
+name = 'happy'
 spotify = spotipy.Spotify(auth=token)
 results = spotify.search(q='playlist:' + name, type='playlist')
 print(results)
